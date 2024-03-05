d53fe5ada8efeab3212700a97849516b21b51f3c
Cihlár Gábor <gabor@cihlar.hu>
2024-03-05 16:13:45 +0100

    Forráskód frissítve

--- pws.py
+++ pws.py
@@ -1,63 +1,65 @@
 # Készítette: Cihlár Gábor
-# Verziószám: 1.0
+# Verziószám: 1.01
+# Frissítve: 2024.03.05-én
 
+# betölti a szükséges bővítményeket
 import requests
 import time
 import schedule
 import arrow
 import json
 
-pws = ""
 # az idézőjelek közé írd be a MET-ÉSZ Beállítások alatt található PWS azonosítót
-jelszo = ""
+pws = ""
 # az idézőjelek közé írd be a MET-ÉSZ jelszavad
-tszfm = 123
+jelszo = ""
 # írd be az egyenlőségjel után 123 helyére a tengerszint feletti magasságot, ahol a műszered található
-kuldes = 300   
+tszfm = 123
 # írd be másodperccel számolva, hogy milyen gyakran szeretnél adatot küldeni (javasolt 300 az 5 perchez, vagy 600 a 10 perchez)
-szenzor = requests.get("http://192.168.0.111/sensors.json")
+kuldes = 300   
 # az IP modul léghőmérséklet, páratartalom és légnyomásmérő érzékelőjének JSON linkje
-szelmero = requests.get("http://192.168.0.111/wind.json")
+szenzor = requests.get("http://192.168.0.111/sensors.json")
 # az IP modul szélmérőjének JSON linkje
+szelmero = requests.get("http://192.168.0.111/wind.json")
     
 def feladat():
     try:
-        idokep1 = json.loads(szenzor.text)
         # az érzékelő adatainak beolvasása
-        oke1 = (szenzor.status_code)
+        idokep1 = json.loads(szenzor.text)
         # az érzékelő válasza
-        tempc = (idokep1['hom'])
+        oke1 = (szenzor.status_code)
         # a léghőmérséklet értéke
+        tempc = (idokep1['hom'])
+        # a műszerszinti légnyomás értéke
         pressure = (idokep1['p'])
-        # a műszerszinti légnyomás értéke
-        humidity = (idokep1['rh'])
         # a páratartalom értéke
-        legnyomas = (idokep1['ap'])
+        humidity = (idokep1['rh'])
         # a műszerszinti légnyomás értéke
+        legnyomas = (idokep1['ap'])
+        # a tengerszinti légnyomás kiszámítása, csak ha van léghőmérséklet adat
         baromhpa = (legnyomas*(1-((0.0065*tszfm)/(tempc+(0.0065*tszfm)+273.15)))**-5.257)
-        # a tengerszinti légnyomás kiszámítása
-        idokep2 = json.loads(szelmero.text)
         # a szélmérő adatainak beolvasása
-        oke2 = (szenzor.status_code)
+        idokep2 = json.loads(szelmero.text)
         # a szélmérő válasza
-        winddir = (idokep2['dir'])
+        oke2 = (szenzor.status_code)
         # a szélirány értéke
-        windspeedkmph = (idokep2['speed']*3.6)
+        winddir = (idokep2['dir'])
         # a szélerősségmérő értéke és kilométe per órás sebesség kiszámítása
-        if oke1 == 200 and oke2 == 200:
+        windspeedkmph = (idokep2['speed']*3.6)
         # ha az érzékelő és a szélmérő válasza megfelelő
+        if oke1 == 200 and oke2 == 200:
+            # akkor kiírja a kapott JSON válaszokat és az OK üzenetet
             print (oke1, oke2, '- OK!')
-            # akkor írja ki a kapott válaszokat és írja ki, hogy OK!
-            utc = time.time()
             # a pontos dátum és idő lekérdezése a számítőgépről
+            utc = time.time()
+            # a dátum és idő átalakítása UTC-ra megfelelő formátumban
             datum = arrow.utcnow().format('YYYY-MM-DD HH:mm:ss')
-            # a dátum és idő átalakítása megfelelő formátumra UTC-ben
+            # előkészíti az API-hoz szükséges formátumot a betöltött és létrehozott adatokkal 
             eszlel = {"PWS": pws, "FIX": jelszo, "dateutc": datum, "pressure": legnyomas, "baromhpa": baromhpa, "tempc": tempc, "humidity": humidity, "winddir": winddir, "windspeedkmph": windspeedkmph}
-            #előkészíti az API-hoz szükséges formátumot a betöltött és létrehozott adatokkal 
+            # elkészíti a beküldéshez szükséges linket az előbb betöltött adatokkal
             bead = requests.post("https://sandman.met.hu/metesz/pws", params=eszlel)
-            #elkészíti a beküldéshez szükséges linket az előbb betöltött adatokkal
+            # kiírja az elkészített linket
             print (bead.url)
-            #kiírja az elkészített linket
         else:
             feladat()
     except json.decoder.JSONDecodeError:
